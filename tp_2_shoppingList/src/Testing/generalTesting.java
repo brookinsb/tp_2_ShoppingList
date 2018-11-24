31929bd53ba7f9eaada79b6957079be3525efb67
Brad <broo2397@stthomas.edu>
2018-11-24 12:42:29 -0800

    Added blanket test to fill.

---
+++ tp_2_shoppingList/src/Testing/generalTesting.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package Testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author bradleybrookins
+ *
+ */
+class generalTesting
+{
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
