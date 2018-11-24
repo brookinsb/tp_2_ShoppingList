59d8e9f0d027411901cc2f808008a78ff9d0b8d5
Brad <broo2397@stthomas.edu>
2018-11-24 12:25:33 -0800

    Added StdLib files.

---
+++ tp_2_shoppingList/src/Testing/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author bradleybrookins
+ *
+ */
+package Testing;
--- tp_2_shoppingList/src/tp_2_shoppingList/mealCollector.java
+++ tp_2_shoppingList/src/tp_2_shoppingList/mealCollector.java
@@ -1,11 +1,13 @@
 package tp_2_shoppingList;
 
+import edu.princeton.cs.introcs.StdOut;
+
 public class mealCollector
 {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+	
 
 	}
 
