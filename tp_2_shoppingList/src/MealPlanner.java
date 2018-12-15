50c990ec44f24792c3d94e46fd2392b7279b77ec
Brad <broo2397@stthomas.edu>
2018-12-14 16:49:21 -0800

    Removed magic numbers. Replaced with constants.

--- tp_2_shoppingList/src/MealPlanner.java
+++ tp_2_shoppingList/src/MealPlanner.java
@@ -2,8 +2,13 @@
 public class MealPlanner {
 	private static String fileName = "MyRecipes.yml";
 
+	private static final int ADD_RECIPE = 1;
+	private static final int ADD_MEAL_PLAN = 2;
+	
+	
 	public static void main(String[] args)
-	{
+	
+	{	
 		boolean done = false;
 		IShopperUI ui = new ShopperUI();
 		MealPrepper mealPrepper = new MealPrepper(ui, fileName);
@@ -16,16 +21,16 @@ public class MealPlanner {
 			
 			switch (task) 
 			{
-			case 1:
+			case ADD_RECIPE:
 				MealCollector mealCollector = new MealCollector(ui, fileName);
 				mealCollector.addRecipe();
 				break;
 				
-			case 2:
+			case ADD_MEAL_PLAN:
 				mealPrepper.selectRecipes();
 				break;
 
-			case 3:
+			case CREATE_SHOPPING_LIST:
 				if (mealPrepper.listMade()) {
 					try {
 						mealPrepper.writeShoppingList();
